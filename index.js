43ca61060c667b5955c8d3d050795c8be05799cc
yoshi-tai <>
2026-04-02 08:00:55 +0900

    update

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-function letsPractice() {
-  location.href = "1page.html";
-}
-
+// textをランダムに表示
 function randomText() {
   const texts = [
     "今日はいい日だ！",
@@ -18,4 +15,19 @@ function randomText() {
   
   // 表示
   document.getElementById("randomText").textContent = texts[randomIndex];
+}
+
+/* 
+ *頑張る？押下→ 頑張る！に変換→ 3秒後遷移
+*/
+function letsPractice() {
+  const btn = document.getElementById("button");
+  
+  // 文字変更
+  btn.value = "無理しないでね！";
+  
+  // 2秒後に遷移
+  setTimeout(function() {
+    location.href = "1page.html";
+  }, 2000);
 }
